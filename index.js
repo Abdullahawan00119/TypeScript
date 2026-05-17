e557d189c2721ffe7eab669f0fcf9887833606a0
Abdullahawan00119 <abdullahawan00119@gmail.com>
2026-05-17 08:59:35 +0500

    typeScript

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+// let sirname: string = "Abdullah"
+// console.log(sirname);
+// let age: number = 20;
+// // age = "ali"
+// console.log(age);
+// let isactive: boolean = true;
+// // isactive =[]
+// console.log(isactive);
+// let nothing : null = null;
+// console.log(nothing);
+// let notdefined : undefined = undefined;
+// console.log(notdefined);
+// Array
+// const fruits: string[] = ["apple", "banana", "watermelon", "pineapple"]
+// console.log(fruits);
+// const numbers : Array<number> = [2,3,4,5];
+// console.log(numbers);
+// const mixedArray : (string | number | boolean)[] = ["Abdullah" , 20  ];
+// console.log(mixedArray);
+// Function 
+// const greet=(name:string , age:number):string=>{
+//      return `My name is ${name} and my age is ${age}`
+// }
+// console.log(greet("Abdullah" , 20));
+// const profile=(name:string , age:number):void=>{
+//      console.log( `My name is ${name} and my age is ${age}`);
+// }
+// profile("Ali Khan" , 40)
+// objects
+const profile = {
+    name: "Abdullah",
+    age: 20,
+    contact: +923178937641,
+    Graduate: false
+};
+console.log(profile);
